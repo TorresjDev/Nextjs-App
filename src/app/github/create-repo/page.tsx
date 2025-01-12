6499d5656774c24c80f2465d043a56645481bdfb
Jesus Torres <j.torres3.dev@gmail.com>
2025-01-11 23:57:41 -0600

    walkthrough next js app

---
+++ src/app/github/create-repo/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Walkthrough from "./walkthrough.mdx";
+
+export default function GithubWalkthroughPage() {
+	return (
+		<main className="prose bg-slate-300 mx-auto p-8 max-w-[50vw]">
+			<Walkthrough />
+		</main>
+	);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,32 @@
+import { nextui } from "@nextui-org/theme";
 import type { Config } from "tailwindcss";
+import Typography from "@tailwindcss/typography";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    darkMode: ["class"],
+    content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/@nextui-org/theme/dist/components/(button|navbar|ripple|spinner).js",
+	],
+	theme: {
+    	extend: {
+    		colors: {
+    			background: 'var(--background)',
+    			foreground: 'var(--foreground)',
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
+    		}
+    	}
     },
-  },
-  plugins: [],
+	plugins: [Typography, nextui()],
 } satisfies Config;
