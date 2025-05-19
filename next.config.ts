17d93c5cf5b793d88e1ed76168f698990e072c9d
Jesus Torres <j.torres3.dev@gmail.com>
2025-05-18 19:45:49 -0500

    mdx rendering update with MDXComponents

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,24 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	reactStrictMode: true,
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+	eslint: {
+		dirs: ["app", "components", "layouts", "scripts"],
+	},
 	images: {
 		domains: ["torresjdev.github.io"],
 	},
 };
 
 const withMDX = createMDX({
-	extension: /\.mdx?$/,
+	extension: /\.(md|mdx)$/,
+	options: {
+		remarkPlugins: [remarkGfm],
+	},
 });
 
 export default withMDX(nextConfig);
--- src/app/guides/[...slug]/page.tsx
+++ src/app/guides/[...slug]/page.tsx
@@ -2,24 +2,34 @@
 import { notFound } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
+import { useMDXComponents } from "../../components/MDXComponents";
 
 export default function WalkthroughPage() {
 	const params = useParams();
 	const slug = params?.slug as string[];
-	const [Component, setComponent] = useState<React.ElementType | null>(null);
+	type MDXContent = React.ComponentType<{
+		components: Record<string, unknown>;
+	}>;
+	const [mdxModule, setMdxModule] = useState<MDXContent | null>(null);
 
+	const components = useMDXComponents({});
 	useEffect(() => {
 		if (!slug || slug.length === 0) return;
 
 		import(`../${slug.join("/")}/walkthrough.mdx`)
-			.then((mod) => setComponent(() => mod.default))
-			.catch(() => setComponent(() => notFound()));
+			.then((mod) => setMdxModule(() => mod.default))
+			.catch(() => setMdxModule(() => notFound()));
 	}, [slug]);
 
 	if (!slug || slug.length === 0) return notFound();
+	if (!mdxModule) return <div className="p-4">Loading...</div>;
 
-	return Component ? (
-		<Component />
+	const Content = mdxModule;
+
+	return Content ? (
+		<div className="prose dark:prose-invert prose-body:text-black max-w-full p-4 ">
+			<Content components={components} />
+		</div>
 	) : (
 		<div className="m-4 text-2xl font-bold">Loading...</div>
 	);
