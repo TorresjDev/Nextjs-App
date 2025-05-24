70ae2076ae0e4448925cd055c9dba0fd0dea79cc
Jesus Torres <j.torres3.dev@gmail.com>
2025-05-24 05:34:58 -0500

    about page fix up

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,14 +6,12 @@ import Image from "next/image";
 export default async function AboutPage() {
 	const profile = await getGitHubProfile();
 
-	console.log("GitHub Profile Data:", profile);
-
 	return (
 		<section
 			id="about"
 			className="w-full mx-auto max-h-[110vh] h-[full] overflow-hidden"
 		>
-			<div className="flex flex-col md:flex-row items-start md:items-center gap-6 my-3">
+			<div className="flex flex-col md:flex-row justify-self-start md:justify-self-center gap-6 my-3">
 				<Image
 					src={profile.avatar_url}
 					alt="GitHub Avatar"
@@ -22,10 +20,10 @@ export default async function AboutPage() {
 					height={65}
 				></Image>
 				<div className="text-center md:text-left">
-					<h1 className="text-4xl md:text-5xl font-semibold text-[#DAA520]">
+					<h1 className="text-3xl md:text-4xl font-semibold text-[#DAA520]">
 						{profile.name}
 					</h1>
-					<p className="text-muted text-sm md:text-xl my-1 md:my-2">
+					<p className="text-muted text-sm md:text-lg my-1 md:my-2">
 						{profile.bio}
 					</p>
 					<section
@@ -62,47 +60,35 @@ export default async function AboutPage() {
 				</div>
 			</div>
 			<p className="text-muted text-sm md:text-xl my-1 md:my-2 text-center">
-				💻 I’m a software engineer with hands-on experience in full-stack
+				💻 I&#39;m software engineer with hands-on experience in full-stack
 				development, system design, and database work. My background in Computer
 				Information Systems gives me the foundation to understand both the
 				technical and business side of software projects.
 			</p>
-			<section className="place-items-center gap-8">
-				{/* <a
-					href="https://gh-stats-gen.vercel.app/"
-					className="w-full max-w-2xl "
-				>
+			<section className="mx-auto max-w-[80vw]">
+				<div className="block lg:flex justify-items-center my-1 w-full mx-auto">
 					<img
-						src="https://github-readme-streak-stats.herokuapp.com/?user=TorresjDev&theme=tokyonight&hide_border=true&ring=820000"
-						alt="GitHub Streak"
-						className="w-full rounded-lg shadow"
-					/>
-				</a> */}
-				<div className="flex gap-2 my-1">
-					<img
-						src="https://github-readme-stats.vercel.app/api?username=TorresjDev&show_icons=true&theme=tokyonight&count_private=true&hide_border=true&hide=contribs"
+						src="https://github-readme-stats.vercel.app/api?username=TorresjDev&show_icons=true&theme=tokyonight&count_private=true&hide=contribs"
 						alt="GitHub Stats"
-						className="w-full max-w-2xl rounded-lg shadow"
+						className="h-[17vh] lg:h-[25vh] w-[60vw] rounded-lg shadow"
 					/>
-
 					<img
-						src="https://github-readme-stats.vercel.app/api/top-langs/?username=TorresjDev&theme=tokyonight&layout=compact&hide_border=true"
+						src="https://github-readme-stats.vercel.app/api/top-langs/?username=TorresjDev&theme=tokyonight&layout=compact"
 						alt="Top Languages"
-						className="w-full max-w-2xl rounded-lg shadow"
+						className="h-[25vh] w-[60vw] lg:w-[40vw] rounded-lg shadow"
 					/>
 				</div>
-
-				{/* <img
-					src="https://github-readme-stats.vercel.app/api/wakatime?username=@@Jtorres&layout=compact&theme=tokyonight&hide_border=true"
-					alt="WakaTime Stats"
-					className="w-full max-w-2xl rounded-lg shadow"
-				/> */}
-
-				<img
-					src="https://ghchart.rshah.org/TorresjDev"
-					alt="GitHub Contributions"
-					className="w-full max-w-4xl rounded-lg shadow"
-				/>
+				<div
+					id="github-contributions"
+					className=" mt-2 md:mt-4 justify-items-center my-1 w-full mx-auto"
+				>
+					<h2 className="font-bold text-lg">Github Contributions</h2>
+					<img
+						src="https://ghchart.rshah.org/TorresjDev"
+						alt="GitHub Contributions"
+						className="w-full max-w-[75vw] xl:max-w-[60vw] rounded-lg shadow p-3 text-bold"
+					/>
+				</div>
 			</section>
 		</section>
 	);
